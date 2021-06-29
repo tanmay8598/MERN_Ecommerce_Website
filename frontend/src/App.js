a24e7bd966035271f3175088246c83b113f1d21f
Tanmay <you@example.com>
2021-06-29 21:15:26 +0530

    Admin delete products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 const App = () => {
   return (
@@ -23,7 +24,6 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/order/:id' component={OrderScreen} />
-
           <Route path='/login' component={LoginScreen} />
           <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/payment' component={PaymentScreen} />
@@ -34,6 +34,7 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -21,6 +21,9 @@ import {
   USER_DELETE_REQUEST,
   USER_DELETE_SUCCESS,
   USER_DELETE_FAIL,
+  USER_EDIT_FAIL,
+  USER_EDIT_SUCCESS,
+  USER_EDIT_REQUEST,
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 export const login = (email, password) => async (dispatch) => {
@@ -229,6 +232,40 @@ export const deleteUser = (id) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: USER_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const editUser = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_EDIT_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/users/${user._id}`, user, config)
+
+    dispatch({
+      type: USER_EDIT_SUCCESS,
+    })
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: USER_EDIT_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
